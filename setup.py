c1c6e0ae43b72675cafd3435d059595e0779ec9d
Kamau <kevinnjagi83@gmail.com>
2025-01-13 13:02:48 +0300

    folder structure set

---
+++ setup.py
@@ -0,0 +1,11 @@
+from gettext import install
+from setuptools import setup, find_packages
+
+setup(
+    name='Medical Chatbot GenAI Project',
+    version='0.0.0',
+    author='Kevin Njagi',
+    author_email='kevinnjagi83@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
